289399d78950cb29f3eb7019cc36a23996f37434
dex2code <dex2code@localhost>
2024-02-06 19:08:48 +0300

    commit

--- remotes/node.py
+++ remotes/node.py
@@ -55,8 +55,9 @@ async def check_node(node_name: str="") -> None:
                 )
             await queue_telegram_message(message_text=t.as_html())
 
-        app_globals.app_results[node_name]['last_status'] = False
-        app_globals.app_results[node_name]['last_result'] = node_result
+        async with app_globals.results_lock:
+            app_globals.app_results[node_name]['last_status'] = False
+            app_globals.app_results[node_name]['last_result'] = node_result
 
     else:
         logger.info(f"Node '{node_name}' ({app_globals.app_results[node_name]['url']}) seems online ({node_chain_id=})")
@@ -73,9 +74,10 @@ async def check_node(node_name: str="") -> None:
                 )
             await queue_telegram_message(message_text=t.as_html())
 
-        app_globals.app_results[node_name]['last_status'] = True
-        app_globals.app_results[node_name]['last_update'] = t_now()
-        app_globals.app_results[node_name]['last_result'] = node_result
+        async with app_globals.results_lock:
+            app_globals.app_results[node_name]['last_status'] = True
+            app_globals.app_results[node_name]['last_update'] = t_now()
+            app_globals.app_results[node_name]['last_result'] = node_result
 
     finally:
         logger.debug(f"API result for node '{node_name}' ({app_globals.app_results[node_name]['url']}):\n{json.dumps(obj=app_globals.app_results[node_name]['last_result'], indent=4)}")
