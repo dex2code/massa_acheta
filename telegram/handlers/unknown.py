195cf9f2f21fbe40514ade7e675ca0f7e9700699
dex2code <dex2code@localhost>
2024-02-13 10:25:17 +0300

    commit

--- telegram/handlers/unknown.py
+++ telegram/handlers/unknown.py
@@ -18,6 +18,9 @@ async def cmd_unknown(message: Message, state: FSMContext) -> None:
     logger.debug("-> Enter Def")
     logger.info(f"-> Got '{message.text}' command from user '{message.chat.id}'")
 
+    if message.chat.id != app_globals.bot.ACHETA_CHAT:
+        return
+
     t = as_list(
         f"⁉ Error: Unknown command \"{message.text}\"", "",
         "👉 Try /help to learn bot commands"
