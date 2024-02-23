6248ed728ddadd7119fc4fc9e749380678590fbf
dex2code <dex2code@localhost>
2024-02-23 22:29:38 +0300

    commit

--- telegram/handlers/chart_wallet.py
+++ telegram/handlers/chart_wallet.py
@@ -15,7 +15,7 @@ import app_globals
 
 from telegram.keyboards.kb_nodes import kb_nodes
 from telegram.keyboards.kb_wallets import kb_wallets
-from tools import get_short_address, check_privacy
+from tools import get_short_address, check_privacy, get_rewards
 
 
 class ChartWalletViewer(StatesGroup):
@@ -242,57 +242,122 @@ async def show_wallet(message: Message, state: FSMContext) -> None:
             },
 
             "scales": {
-                "xAxes": [ { "stacked": True } ],
+
                 "yAxes": [
                     {
+                        "id": "blocks",
+                        "display": True,
+                        "position": "left",
                         "stacked": True,
-                        "ticks": { "min": 0 }
+                        "ticks": { "min": 0, "fontColor": "black" },
+                        "gridLines": { "drawOnChartArea": False }
+                    },
+                    {
+                        "id": "earnings",
+                        "display": True,
+                        "position": "right",
+                        "stacked": False,
+                        "ticks": { "fontColor": "blue" },
+                        "gridLines": { "drawOnChartArea": False }
                     }
-                ]
+                ],
+                "xAxes": [ { "stacked": True } ]
             }
         },
 
         "data": {
+
             "labels": [],
+
             "datasets": [
                 {
+                    "type": "line",
+                    "label": "Estimated earnings (MAS)",
+                    "yAxisID": "earnings",
+                    "lineTension": 0.4,
+                    "fill": False,
+                    "borderColor": "blue",
+                    "borderWidth": 2,
+                    "pointRadius": 0,
+                    "data": []
+                },
+                {
+                    "type": "bar",
                     "label": "Operated blocks",
+                    "yAxisID": "blocks",
                     "backgroundColor": "green",
                     "data": []
                 },
                 {
+                    "type": "bar",
                     "label": "Missed blocks",
+                    "yAxisID": "blocks",
                     "backgroundColor": "red",
                     "data": []
-                }
+                },
             ]
         }
     }
 
     try:
-        wallet_stat_cycles = {}
+        wallet_stat_keytime_unsorted = {}
         for measure in app_globals.app_results[node_name]['wallets'][wallet_address]['stat']:
-            wallet_stat_cycles[measure['cycle']] = {
-                "balance": measure['balance'],
-                "rolls": measure['rolls'],
-                "ok_blocks": measure['ok_blocks'],
-                "nok_blocks": measure['nok_blocks']
+            measure_time = measure['time']
+            measure_cycle = measure['cycle']
+            measure_balance = measure['balance']
+            measure_rolls = measure['rolls']
+            measure_ok_blocks = measure['ok_blocks']
+            measure_nok_blocks = measure['nok_blocks']
+
+            wallet_stat_keytime_unsorted[measure_time] = {
+                "cycle": measure_cycle,
+                "balance": measure_balance,
+                "rolls": measure_rolls,
+                "ok_blocks": measure_ok_blocks,
+                "nok_blocks": measure_nok_blocks
             }
 
-        wallet_stat_cycles = dict(sorted(wallet_stat_cycles.items()))
-        for cycle in wallet_stat_cycles:
-            balance = wallet_stat_cycles[cycle]['balance']
-            rolls = wallet_stat_cycles[cycle]['rolls']
-            ok_blocks = wallet_stat_cycles[cycle]['ok_blocks']
-            nok_blocks = wallet_stat_cycles[cycle]['nok_blocks']
+        wallet_stat_keytime_sorted = dict(
+            sorted(
+                wallet_stat_keytime_unsorted.items()
+            )
+        )
+
+        wallet_stat_keycycle_unsorted = {}
+        for measure in wallet_stat_keytime_sorted:
+            measure_cycle = wallet_stat_keytime_sorted[measure]['cycle']
+            measure_balance = wallet_stat_keytime_sorted[measure]['balance']
+            measure_rolls = wallet_stat_keytime_sorted[measure]['rolls']
+            measure_ok_blocks = wallet_stat_keytime_sorted[measure]['ok_blocks']
+            measure_nok_blocks = wallet_stat_keytime_sorted[measure]['nok_blocks']
+
+            wallet_stat_keycycle_unsorted[measure_cycle] = {
+                "balance": measure_balance,
+                "rolls": measure_rolls,
+                "ok_blocks": measure_ok_blocks,
+                "nok_blocks": measure_nok_blocks
+            }
+
+        wallet_stat_keycycle_sorted = dict(
+            sorted(
+                wallet_stat_keycycle_unsorted.items()
+            )
+        )
+
+        for cycle in wallet_stat_keycycle_sorted:
+            balance = wallet_stat_keycycle_sorted[cycle]['balance']
+            rolls = wallet_stat_keycycle_sorted[cycle]['rolls']
+            ok_blocks = wallet_stat_keycycle_sorted[cycle]['ok_blocks']
+            nok_blocks = wallet_stat_keycycle_sorted[cycle]['nok_blocks']
 
             staking_chart_config['data']['labels'].append(cycle)
             staking_chart_config['data']['datasets'][0]['data'].append(rolls)
             staking_chart_config['data']['datasets'][1]['data'].append(balance)
 
             blocks_chart_config['data']['labels'].append(cycle)
-            blocks_chart_config['data']['datasets'][0]['data'].append(ok_blocks)
-            blocks_chart_config['data']['datasets'][1]['data'].append(nok_blocks)
+            blocks_chart_config['data']['datasets'][0]['data'].append(get_rewards(rolls_number=rolls))
+            blocks_chart_config['data']['datasets'][1]['data'].append(ok_blocks)
+            blocks_chart_config['data']['datasets'][2]['data'].append(nok_blocks)
 
         staking_chart = QuickChart()
         staking_chart.device_pixel_ratio = 2.0
