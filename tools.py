3d0d1d1ea892c554a6fcf8243a4c9ea49770eaf0
dex2code <dex2code@localhost>
2024-02-26 18:19:07 +0300

    commit

--- tools.py
+++ tools.py
@@ -243,7 +243,7 @@ async def get_rewards_mas(rolls_number: int=0) -> int:
 
 
 @logger.catch
-async def get_rewards_blocks(rolls_number: int=0) -> int:
+async def get_rewards_blocks(rolls_number: int=0) -> float :
     logger.debug("-> Enter Def")
 
     if app_globals.massa_network['values']['total_staked_rolls'] == 0 or rolls_number == 0:
@@ -252,7 +252,7 @@ async def get_rewards_blocks(rolls_number: int=0) -> int:
     else:
         my_contribution = app_globals.massa_network['values']['total_staked_rolls'] / rolls_number
         my_blocks = round(
-            172_800 / my_contribution,
+            172_800 / my_contribution / 86_400 * 2_048,
             4
         )
 
